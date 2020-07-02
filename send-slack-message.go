085daa8fca6ea272989b5de67c71a6a1eecc0f06
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-02 16:19:03 +0300

    Update send-slack-message.go
    add header send

--- send-slack-message.go
+++ send-slack-message.go
@@ -8,6 +8,7 @@ import(
   "fmt"
   "bytes"
   "encoding/json"
+  "time"
 )
 
 type page struct {}
@@ -22,10 +23,24 @@ func (p page) ServeHTTP (w http.ResponseWriter, _ *http.Request){
    "channel": "#general",
    "text": "test hello",
   })
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  timeout := time.Duration(5*time.Second)
+  client := http.Client{
+    Timeout: timeout,
+  }
+  request, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+  if err != nil {
+    log.Fatalln(err)
+  }
+  request.Header.Set("Content-Type","application/json;charset=utf-8")
+
   //resp, err := http.Get("http://httpbin.org/get")
   //resp, err := http.Post("http://httpbin.org/post","application/json",bytes.NewBuffer(requestBody))
-  resp, err := http.Post(url,"application/json",bytes.NewBuffer(requestBody))
-  
+  //resp, err := http.Post(url,"application/json",bytes.NewBuffer(requestBody))
+  resp, err := client.Do(request)
 
   if err != nil {
     log.Fatalln(err)
