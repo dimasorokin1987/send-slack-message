a5bd87813408ce8b0bbd73d022925b72ba20b067
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-02 14:49:06 +0300

    Create send-slack-message.go

---
+++ send-slack-message.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+  //"os"
+  "io/ioutil"
+  "log"
+  "net/http"
+)
+
+type page struct {}
+func (p page) ServeHTTP (w http.ResponseWriter, _ *http.Request){
+  //key := os.Getenv("SLACK_SECRET_KEY")
+  //url := 
+  resp, err := http.Get("http://httpbin.org/get")
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer resp.Body.Close()
+  
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  //log.Println(string(body))
+  fmt.Fprint(w,string(body))
+}
+
+func main(){
+  var p page
+  err := http.ListenAndServe(":"+os.Getenv("PORT"),p)
+  if err!=nil {
+    log.Fatalln(err)
+  }
+}
