9b8f379017870913bbcd8cbdd1126b70000ed4c2
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-03 12:47:59 +0300

    Update send-slack-message.go
    fix

--- send-slack-message.go
+++ send-slack-message.go
@@ -13,8 +13,11 @@ import(
 
 type page struct {}
 func (p page) ServeHTTP (w http.ResponseWriter, r *http.Request){
-  if origin := req.Header.Get("Origin"); origin != "" {
-    w.Header().Set("Access-Control-Allow-Origin", "https://dimasorokin1987.github.io")
+//   if origin != "https://dimasorokin1987.github.io"{
+//     return
+//   }
+  if origin := r.Header.Get("Origin"); origin != "" {
+    w.Header().Set("Access-Control-Allow-Origin", origin)
     w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
     w.Header().Set("Access-Control-Allow-Headers",
         "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
